bb03848ac09c21a20a8a1baad3d692c4766b418e
jle2320 <emzs1996@gmail.com>
2026-06-23 13:57:42 +0800

    Update DB

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 require("dotenv").config();
 const connection = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: "localhost",
+  user: "u867012074_mpsac",
+  password: "mercyGwapa101",
+  database: "u867012074_mpsac"
 });
 
 
